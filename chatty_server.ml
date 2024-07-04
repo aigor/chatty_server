20ce56198659c49a58d5143e554bb32d42af826f
aigooor <2151715+aigor@users.noreply.github.com>
2024-07-04 22:17:00 +0300

    Basic TCP server

--- chatty_server.ml
+++ chatty_server.ml
@@ -1,8 +1,12 @@
 let default_child_prcesses = 16
+let application_port = 54321
+
 let child_processes = App_args.resolve_app_arguments_or_exit default_child_prcesses
 
 let () =
   Logging.setup_logging_infrastructure ~log_filename:"application.log";
-  
+  Logs.info (fun m -> m "=== Chatty Server ===");
   Logs.info (fun m -> m "Chatting with sometimes faulty chield processes (%i processes)" child_processes);
 
+  let serve = Tcp_server.create_socket_server application_port in
+  Lwt_main.run @@ serve ()
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-dune exec -w -- ./chatty_server.exe 25
+dune exec -- ./chatty_server.exe 27
