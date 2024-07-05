2290babd8ed99a60c56a39501620db84282d0734
aigooor <2151715+aigor@users.noreply.github.com>
2024-07-05 11:40:44 +0300

    Added mesage logging

--- tcp_server.ml
+++ tcp_server.ml
@@ -11,26 +11,34 @@ let handle_incoming_message msg =
     | "KEEP_ALIVE" -> Some "KEEP_ALIVE_ACK"
     | _ -> None
 
-let rec handle_connection ic oc () =
+let rec handle_connection ic oc connection_id () =
     Lwt_io.read_line_opt ic >>=
     (fun incoming_msg -> match incoming_msg with
         | Some incoming_msg -> 
             (
-                Logs.info(fun m -> m "[TODO: Add connection id] << Incoming message: %s" incoming_msg);
+                Logs.info(fun m -> m "[%i] << %s" connection_id incoming_msg);
                 match handle_incoming_message incoming_msg with
                 | Some msg -> (
-                    Logs.info(fun m -> m "[TODO: Add connection id] >> Outgoing message: %s" msg);
-                    Lwt_io.write_line oc msg >>= handle_connection ic oc
+                    Logs.info(fun m -> m "[%i] >> %s" connection_id msg);
+                    Lwt_io.write_line oc msg >>= handle_connection ic oc connection_id
                 )
-                | None -> handle_connection ic oc ())
-        | None -> Logs_lwt.info (fun m -> m "Connection closed") >>= return)
+                | None -> handle_connection ic oc connection_id ())
+        | None -> Logs_lwt.info (fun m -> m "[%i] Connection closed" connection_id) >>= return)
 
+let nex_connection_id =
+  let counter = ref 0 in
+  fun () ->
+    incr counter;
+    !counter
+        
+        
 let accept_connection conn =
+    let connection_id = nex_connection_id () in
     let fd, _ = conn in
     let ic = Lwt_io.of_fd ~mode:Lwt_io.Input fd in
     let oc = Lwt_io.of_fd ~mode:Lwt_io.Output fd in
-    Lwt.on_failure (handle_connection ic oc ()) (fun e -> Logs.err (fun m -> m "%s" (Printexc.to_string e) ));
-    Logs_lwt.info (fun m -> m "New connection") >>= return
+    Lwt.on_failure (handle_connection ic oc connection_id ()) (fun e -> Logs.err (fun m -> m "%s" (Printexc.to_string e) ));
+    Logs_lwt.info (fun m -> m "[%i] New connection" connection_id) >>= return
 
 let create_socket port =
     let open Lwt_unix in
