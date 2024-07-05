49af86d35abe86712bf1be8479f44cdcc24c7fb1
aigooor <2151715+aigor@users.noreply.github.com>
2024-07-05 17:00:43 +0300

    Handling KEEP_AVLIVE in client

--- child_process_manager.ml
+++ child_process_manager.ml
@@ -1,42 +1,18 @@
-(* TODO: Child process implementation here *)  
 open Lwt
-
-
-(*
-
-   let rec handle_connection ic oc connection_id () =
-    Lwt_io.read_line_opt ic >>=
-    (fun incoming_msg -> match incoming_msg with
-        | Some incoming_msg -> 
-            (
-                Logs.info(fun m -> m "[%i] << %s" connection_id incoming_msg);
-                match handle_incoming_message incoming_msg with
-                | Some msg -> (
-                    Logs.info(fun m -> m "[%i] >> %s" connection_id msg);
-                    Lwt_io.write_line oc msg >>= handle_connection ic oc connection_id
-                )
-                | None -> handle_connection ic oc connection_id ())
-        | None -> Logs_lwt.info (fun m -> m "[%i] Connection closed" connection_id) >>= return)
-
-    Lwt.on_failure (handle_connection ic oc connection_id ()) (fun e -> Logs.err (fun m -> m "%s" (Printexc.to_string e) ));
-    Logs_lwt.info (fun m -> m "[%i] New connection" connection_id) >>= return
-
-
-*)
 
 let rec handle_process_output ic process_id () =
   Lwt_io.read_line_opt ic >>=
   (fun output_msg -> match output_msg with
       | Some msg -> (
-          Logs.info(fun m -> m "[process-%i] ### %s" process_id msg); 
-          handle_process_output ic process_id ())
+          Logs_lwt.info(fun m -> m "[process-%i] ### %s" process_id msg) >>= 
+          handle_process_output ic process_id)
       | None -> Logs_lwt.info (fun m -> m "[process-%i] Process exited, restarting..." process_id) >>= return)
 
 let spawn_child_processes app_name process_amount host_application_tcp_port = 
   let proces_id = 10 in
   Logs.info (fun m -> m "Starting %i child processes to connect to TCP server on port %i" process_amount host_application_tcp_port);
   (* TODO: Use actual required child amount *)
-  (* TODO: Handle application termination *)
+  (* TODO: Handle child process termination & respawn *)
 
   let child_process_command = app_name ^ " -child " ^ string_of_int host_application_tcp_port in
   Logs.info (fun m -> m "[process-%i] Starting child process with command: %s" proces_id child_process_command);
--- tcp_client.ml
+++ tcp_client.ml
@@ -1,40 +1,52 @@
-(* Client mode: connect to TCP server and communicate with it *)
+(**
+   Client mode: 
+   Connects to the TCP server and communicate with it.
+   When receives a message from the TCP server it replies with the ACK_RECEIVED <original-message-length> message.  
+   Sends KEEP_ALIVE messages at randlon 5 to 20 seconds interval.
+   Finishes process execution at random interval between 60 to 120 seconds.
+*)
 
 open Lwt
 
-(* TODO: Add message handler *)
-(* TODO: Add KEEP_ALIVE with random interval 5 to 20 seconds *)
-(* TODO: Finish process in random interval from 60 to 120 seconds *)
+let random_float_from_interval from_value to_value =
+  float_of_int (from_value + Random.int (to_value - from_value))
 
 let handle_incoming_message msg =
   match msg with
   | "KEEP_ALIVE_ACK" -> None
   | msg -> Some ("ACK_RECEIVED " ^ string_of_int (String.length msg))
 
+let rec keep_alive_handler oc () = 
+  let keep_alive = "KEEP_ALIVE" in  
+  Lwt.bind (Lwt_unix.sleep (random_float_from_interval 1 5)) 
+    (fun () -> Lwt_io.write_line oc keep_alive >>= keep_alive_handler oc)
+
 let rec handle_connection ic oc () =
   Lwt_io.read_line_opt ic >>=
   (fun incoming_msg -> match incoming_msg with
       | Some incoming_msg -> 
           (
-              Logs.info(fun m -> m "[child] << %s" incoming_msg);
               match handle_incoming_message incoming_msg with
-              | Some msg -> (
-                  Logs.info(fun m -> m "[child] >> %s" msg);
-                  Lwt_io.write_line oc msg >>= handle_connection ic oc
-              )
+              | Some msg -> Lwt_io.write_line oc msg >>= handle_connection ic oc
               | None -> handle_connection ic oc ())
-      | None -> Logs_lwt.info (fun m -> m "Connection closed") >>= return)
+      | None -> Logs_lwt.info (fun m -> m "Server unexpectedly closed connection") >>= 
+         fun () -> Lwt.fail Exit >>= return)
 
 let client_connect host port =
-  Lwt_io.printf "Connecting to %s:%d...\n" host port >>= fun () ->
-    let sockaddr = Unix.ADDR_INET (Unix.inet_addr_of_string host, port) in
-    let sock = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
-    Lwt_unix.connect sock sockaddr >>= fun () ->
-    let ic = Lwt_io.of_fd ~mode:Lwt_io.input sock in
-    let oc = Lwt_io.of_fd ~mode:Lwt_io.output sock in
-    handle_connection ic oc () >>= fun () ->
-    Lwt_unix.close sock
+  let sockaddr = Unix.ADDR_INET (Unix.inet_addr_of_string host, port) in
+  let sock = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
+  Lwt_unix.connect sock sockaddr >>= fun () ->
+  let ic = Lwt_io.of_fd ~mode:Lwt_io.input sock in
+  let oc = Lwt_io.of_fd ~mode:Lwt_io.output sock in
+  Lwt.async (keep_alive_handler oc);
+  handle_connection ic oc () >>= fun () ->
+  Lwt_unix.close sock
+
+let register_future_child_process_termination () =
+  Lwt.bind (Lwt_unix.sleep (random_float_from_interval 30 40)) 
+    (fun () -> Lwt.fail Exit)
 
 let start_tcp_client application_port =
+  Lwt.async (register_future_child_process_termination);
   let host = "127.0.0.1" in
   client_connect host application_port
